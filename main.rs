115ad98878bcbc4a6e0d5e5cb48d5791477a9864
Berk <bkaan721@gmail.com>
2025-12-22 21:14:21 +0300

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,22 @@
+mod gui;
+mod simulation;
+mod structs;
+mod report; // for export_pdf  
+
+use eframe::egui;          // for sharpness ı add ıt 
+use crate::gui::DLSApp;
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1500.0, 1000.0])
+            .with_min_inner_size([1200.0, 800.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Dynamic Light Scattering Simulator",
+        options,
+        Box::new(|cc| Ok(Box::new(DLSApp::new(cc)))),
+    )
+}
